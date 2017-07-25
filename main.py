25d9f39a6ffecc055e6131aadd13c9bce2440813
benstox <benstox@gmail.com>
2017-07-25 15:35:03 +0100

    Fixed bug.

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ WIKIPEDIA_URL = "https://en.wikipedia.org"
 CATEGORY_URLS = [
     "https://en.wikipedia.org/wiki/Category:English_cardinals",
     "https://en.wikipedia.org/wiki/Category:19th-century_cardinals",
+    "https://en.wikipedia.org/wiki/Category:French_cardinals",
+    "https://en.wikipedia.org/wiki/Category:Italian_cardinals",
+    "https://en.wikipedia.org/wiki/Category:German_cardinals",
 ]
 
 CATEGORY_INSTANCE_CSS = "#mw-pages div.mw-category li a"
 INFOBOX_CSS = "table.infobox tr"
 
+cardinals_by_nationality = "https://en.wikipedia.org/wiki/Category:Cardinals_by_nationality"
 newman_url = "https://en.wikipedia.org/wiki/John_Henry_Newman"
 
 prelate_urls = set()
@@ -37,7 +41,7 @@ for category_url in CATEGORY_URLS:
         prelate_r = requests.get(prelate_link)
         prelate_s = BeautifulSoup(prelate_r.content, "lxml")
         prelate_name = prelate_s.h1.text
-        infobox_fields = s.select(INFOBOX_CSS)
+        infobox_fields = prelate_s.select(INFOBOX_CSS)
         motto_fields = [
             tr for tr in infobox_fields
             if tr.th and "motto" in tr.th.text.lower().strip()]
